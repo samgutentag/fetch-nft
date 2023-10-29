a1f7672cf78adba5b9343b6e6f3fd04833e4bd32
Sam Gutentag <1404219+samgutentag@users.noreply.github.com>
2023-10-28 23:38:46 -0700

    remove trailing slash from config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "samgutentag", // Usually your GitHub org/user name.
   projectName: "fetch-nft", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
