5ca79c6df8195e03539d427969c0c6aa7bd22e96
Sam Gutentag <1404219+samgutentag@users.noreply.github.com>
2023-10-28 20:10:03 -0700

    init commit gh-pages branch

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,11 +7,11 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Fetch-NFT",
-  tagline: "keep trying to make fetch happen",
+  tagline: "trying to make fetch happen",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "samgutentag.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/fetch-nft/",
